a9e1e5275a90dc071cab75d73f023b7e0379d02f
oxananazaruk <kurylooxana99@gmail.com>
2024-09-23 11:01:23 +0300

    edited build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+    },
+  },
   base: "/events-app/",
 });
