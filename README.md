87aba82534b3a2588d3a5434dcb7b7a96735582d
oxananazaruk <kurylooxana99@gmail.com>
2024-09-23 14:07:19 +0300

    import edited

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Level middle
 
 - [@frontend code link](https://github.com/oxananazaruk/events-app) in GitHub
-- [@frontend ui link](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md)
+- [@frontend ui link](https://oxananazaruk.github.io/event-planner)
 
 - [@backend code link](https://github.com/oxananazaruk/events-be) in GitHub
 - [@backend deploy link](https://events-be-v7ja.onrender.com) in render.com
--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -7,32 +7,6 @@ import {
   TitleName,
 } from "./EventsCard.styled";
 
-// import { Link } from "react-router-dom";
-
-// export const EventCard = ({ event }) => {
-//   const { title, eventDate, description, organizer } = event;
-
-//   return (
-//     <div>
-//       <div>
-//         <h2>{title}</h2>
-//         <p>{description}</p>
-//         <p>{eventDate}</p>
-//         <p>{organizer}</p>
-//       </div>
-//       <div>
-//         <Link to={`/registration`} state={{ event }}>
-//           Register
-//         </Link>
-
-//         <Link to={`/participants`} state={{ event }}>
-//           View
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
 export const EventCard = ({ event }) => {
   const { title, eventDate, description, organizer } = event;
 
